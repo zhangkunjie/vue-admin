a48f67a8291b68f78d6f0c635d4dd02bc869a286
张坤杰 <zhangkunjie@zhangkunjiedeMacBook-Pro.local>
2019-04-28 17:21:28 +0800

    aa

--- src/views/nav2/Page4.vue
+++ src/views/nav2/Page4.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <h1>vuex 测试</h1>
@@ -10,7 +11,6 @@
 <script>
   import { mapGetters } from 'vuex'
   import { mapActions } from 'vuex'
-
   export default {
     computed: {
   	// 使用对象展开运算符将 getters 混入 computed 对象中
@@ -29,5 +29,4 @@
       //})
     }
   }
-
 </script>
